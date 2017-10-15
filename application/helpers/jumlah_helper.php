edfa4002f390782a992a845ea3c635c5fad052dc
mulqan <mulqanadi@gmail.com>
2017-10-15 18:29:22 +0700

    Version 0

---
+++ application/helpers/jumlah_helper.php
@@ -0,0 +1,13 @@
+<?php
+if(!function_exists('jumlah')){
+  function jumlah ($a, $b){
+    return $a + $b;
+  }
+}
+
+if(!function_exists('kurang')){
+  function kurang ($a, $b){
+    return $a - $b;
+  }
+}
+ ?>
